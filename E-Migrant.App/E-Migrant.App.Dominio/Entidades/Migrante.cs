1c3cb1f36018446d3fa0022351220eb38f3fa8fd
Luis Fernández <luisexneider1999@gmail.com>
2021-11-26 12:11:47 -0500

    Se agrego entidad Migrante en la capa de Dominio

---
+++ E-Migrant.App/E-Migrant.App.Dominio/Entidades/Migrante.cs
@@ -0,0 +1,20 @@
+using System;
+namespace E_Migrant.App.Dominio.Entidades
+{
+    public class Migrante
+    {
+        public int id {get; set;}
+        public string nombre {get; set;}
+        public string apellidos {get; set;}
+        public tipoIdentificacion tipoIdentificacion {get; set;} 
+        public string numeroIdentificacion {get; set;}
+        public string pais {get; set;}
+        public DateTime fechaNacimiento {get;set;}
+        public string correoElectronico {get;set;}
+        public string numeroTelefonico {get; set;}
+        public string direccionActual {get;set;}
+        public string ciudad {get; set;}
+        public string situacionLaboral {get; set;}
+
+    }
+}
