1783cfe4f7ef7917daa7d951b7af4eadafa6f343
Mauricio Roa <mauricioroa@iMac-de-Mauricio.local>
2021-11-26 14:53:06 -0500

    reto 0 IMC

--- Sln.App/Grupo14.App.Dominio/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Grupo14.App.Dominio;
-public class Class1
-{
-
-}
---
+++ reto0IMC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace reto0IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peso: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Altura: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            var imc = p/(h*h);
+
+            if (imc < 16)
+            {
+                Console.WriteLine("Su IMC es: " + imc + ". Categoria: Delgadez severa");
+            }
+            else if (imc < 16.99 && imc > 16)
+            {
+                Console.WriteLine("Su IMC es: " + imc + ". Categoria: Delgadez moderada");
+            }
+            else if (imc < 18.49 && imc > 17)
+            {
+                Console.WriteLine("Su IMC es: " + imc + ". Categoria: Delgadez aceptable");
+            }
+            else if (imc < 24.99 && imc > 18.5)
+            {
+                Console.WriteLine("Su IMC es: " + imc + ". Categoria: Peso normal");
+            }
+            else if (imc < 29.99 && imc > 25)
+            {
+                Console.WriteLine("Su IMC es: " + imc + ". Categoria: Sobrepeso");
+            }
+        }
+    }
+}
