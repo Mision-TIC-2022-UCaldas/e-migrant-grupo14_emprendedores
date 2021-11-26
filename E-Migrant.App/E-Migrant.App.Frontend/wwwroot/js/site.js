fa3c4617335f290edff5f720cd089b50a909cf43
Mauricio Roa <mauricioroa@iMac-de-Mauricio.local>
2021-11-26 15:53:13 -0500

    Creacion de formulario registro imigrantes

--- E-Migrant.App/E-Migrant.App.Frontend/wwwroot/js/site.js
+++ E-Migrant.App/E-Migrant.App.Frontend/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }else{
+            RegistrarPersona();
+            event.preventDefault()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
