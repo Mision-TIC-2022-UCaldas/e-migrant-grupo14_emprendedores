a7912957e13079bdc0c52e0e71346b9546be8303
Luis Fernández <luisexneider1999@gmail.com>
2021-11-27 22:51:42 -0500

    Reto33

--- E-Migrant.App/E-Migrant.App.Frontend/Pages/RegistrarDatosEntidad/RegistrarDatosEntidad.cshtml.cs
+++ E-Migrant.App/E-Migrant.App.Frontend/Pages/RegistrarDatosEntidad/RegistrarDatosEntidad.cshtml.cs
@@ -11,21 +11,32 @@ using E_Migrant.App.Persistencia;
 
 namespace E_Migrant.App.Frontend
 {
-   
+
     public class RegistrarDatosEntidadModel : PageModel
-    {   
-        
+    {
+
         private static IRepositorioDatosEntidadesColaboradoras _repoDatosEnti = new RepositorioDatosEntidadesColaboradores(new Persistencia.AppContext());
-        public DatosEntidadesColaboradoras datosEntidadesColaboradoras {get; set;}
+        public DatosEntidadesColaboradoras datosEntidadesColaboradoras { get; set; }
         public IActionResult OnPost(DatosEntidadesColaboradoras datosEntidadesColaboradoras)
         {
-            var adicionando= _repoDatosEnti.AddDatosEntidades(datosEntidadesColaboradoras);
-            if(adicionando== null){
+            if (datosEntidadesColaboradoras.serviciosOfrece == "Tipo Servicio" || datosEntidadesColaboradoras.sector == "sector")
+            {
                 return RedirectToPage("../Error");
-            }else{
-                datosEntidadesColaboradoras= adicionando;
-                return RedirectToPage("../index");
             }
+            else
+            {
+                var adicionando = _repoDatosEnti.AddDatosEntidades(datosEntidadesColaboradoras);
+                if (adicionando == null)
+                {
+                    return RedirectToPage("../Error");
+                }
+                else
+                {
+                    datosEntidadesColaboradoras = adicionando;
+                    return RedirectToPage("../index");
+                }
+            }
+
         }
     }
 }
