a7225d5ed02be6345572c82e49ed711129939479
Luis Fernández <luisexneider1999@gmail.com>
2021-11-27 20:41:10 -0500

    daño

---
+++ E-Migrant.App/E-Migrant.App.Dominio/Entidades/DatosEntidadesColaboradoras.cs
@@ -0,0 +1,19 @@
+using System;
+namespace E_Migrant.App.Dominio{
+
+    public class DatosEntidadesColaboradoras{
+        public int id {get; set;}
+
+        public string razonSocial {get; set;}
+        public string nit {get; set;}
+        public string direccion {get; set;}
+        public string ciudad {get; set;}
+        public string Telefono {get; set;}
+        public string correoElectronico {get; set;}
+
+        public string paginaWeb {get; set;}
+        public string sector {get; set;}
+        public string serviciosOfrece {get;set;}
+        
+    }
+}
